766667f47264d7ce4a998726286d74deb9abc67d
Kauts Kanu <kautscoding@gmail.com>
2020-01-16 18:51:34 +0530

    Initial Commit

---
+++ CustomImageView/CustomImageView.h
@@ -0,0 +1,19 @@
+//
+//  CustomImageView.h
+//  CustomImageView
+//
+//  Created by Kautsya Kanu on 16/01/20.
+//  Copyright © 2020 Kautsya Kanu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomImageView.
+FOUNDATION_EXPORT double CustomImageViewVersionNumber;
+
+//! Project version string for CustomImageView.
+FOUNDATION_EXPORT const unsigned char CustomImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomImageView/PublicHeader.h>
+
+
