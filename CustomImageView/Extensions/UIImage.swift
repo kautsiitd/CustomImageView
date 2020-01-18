6fab353fd97b126adde45e9f08f4ac19f27a3ed8
Kauts Kanu <kautscoding@gmail.com>
2020-01-18 23:58:06 +0530

    Added extension for image resizing, need to handle threading issue

--- CustomImageView/Extensions/UIImage.swift
+++ CustomImageView/Extensions/UIImage.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 extension UIImage {
-    
+    func resize(to size: CGSize) -> UIImage {
+        let imageRenderer = UIGraphicsImageRenderer(size: size, format: imageRendererFormat)
+        let reducedImage = imageRenderer.image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return reducedImage
+    }
 }
